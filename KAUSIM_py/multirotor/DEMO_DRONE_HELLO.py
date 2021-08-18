88fe3ccad7c09a0323477a5b718b58424947ee65
RETELLIGENCE-IWEN <iwinstudioi@gmail.com>
2021-08-19 05:03:52 +0900

    Create DEMO_DRONE_HELLO.py
    Added For Demo

---
+++ KAUSIM_py/multirotor/DEMO_DRONE_HELLO.py
@@ -0,0 +1,28 @@
+import setup_path 
+import airsim
+
+import numpy as np
+import os
+import tempfile
+import pprint
+import cv2
+
+import time
+
+# connect to the AirSim simulator
+client = airsim.MultirotorClient()
+client.confirmConnection()
+client.enableApiControl(True)
+client.armDisarm(True)
+
+wind = airsim.Vector3r(0,-13,0)
+# client.simSetWind(wind)
+
+
+
+
+# airsim.wait_key('Press any key to takeoff')
+client.takeoffAsync().join()
+client.moveToPositionAsync(22, -10, -25, 5).join()
+# state = client.getMultirotorState()
+# print("state: %s" % pprint.pformat(state))
