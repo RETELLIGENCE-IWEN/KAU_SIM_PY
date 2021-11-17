7ae088bb88d0688b1b307d7e77bebb4f4b8687e8
RETELLIGENCE-IWEN <iwinstudioi@gmail.com>
2021-11-18 02:17:30 +0900

    Update headings.py

--- KAUSIM_py/multirotor/common/headings.py
+++ KAUSIM_py/multirotor/common/headings.py
@@ -24,3 +24,8 @@ EMERGENCY_LANDING = "EMERGENCY_LANDING"
 WAYPOINT_REACHED = "WAYPOINT_REACHED"
 RUNNING_STATE = "RUNNING_STATE"
 # 0: Normal, 1: Time Out, 2: Location Out
+
+
+#Custom
+Desired_Wind = "Desired_Wind"
+Desired_Error_Strength = "Desired_Error_Strength"
